715be59dbefd565a3f25c02b0444c46202fdf0a1
kdh0711 <kdh0711@somansa.com>
2024-01-04 16:50:49 +0900

    feat: mainPage

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,8 +1,33 @@
 import userData from "../../mock/mockUserData.json";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useEffect } from "react";
+
+interface HomeworkData {
+  done: number;
+  assigned: number;
+}
+
+interface UserData {
+  name: string;
+  useremail: string;
+  homework: Record<string, HomeworkData>;
+}
 
 export default function Main() {
-  const homeworkDateKeys = Object.keys(userData.homework);
-  const latestData = userData.homework[homeworkDateKeys[homeworkDateKeys.length - 1]];
+  const user: UserData = userData as UserData;
+  const latestHomeworkDate = Math.max(...Object.keys(user.homework).map(Number));
+  const latestData = user.homework[latestHomeworkDate.toString()];
+
+  const getTopics = async () => {
+    const snapshot = await getDocs(collection(db, "topics"));
+    return snapshot;
+  };
+
+  useEffect(() => {
+    console.log(getTopics());
+  }, []);
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <h1>안녕하세요, {userData.name} 님!</h1>
