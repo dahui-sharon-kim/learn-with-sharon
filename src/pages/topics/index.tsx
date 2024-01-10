9b0eeb1149f667ec9b8e56532aba6c3026666120
kdh0711 <kdh0711@somansa.com>
2024-01-10 17:49:15 +0900

    feat: change calendar input type

--- src/pages/topics/index.tsx
+++ src/pages/topics/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { collection, getDocs, query, where, documentId } from "firebase/firestore";
 import { db } from "../../firebase";
+import dayjs from "dayjs";
 
 interface Topic {
   uid: string;
@@ -41,29 +42,30 @@ export default function Topics() {
     const formObject = Object.fromEntries(formData.entries());
     const groupName = formObject.groupName as string;
     const date = formObject.date as string;
-    const keyArr = await getHomeworkKeyArr(groupName, date);
+    const dateFormatted = date.replaceAll("-", "");
+    const keyArr = await getHomeworkKeyArr(groupName, dateFormatted);
     setHomeworkData(await getHomeworkDataFromKey(keyArr));
   };
 
   return (
     <div className="w-full h-full max-w-5xl p-8 flex flex-col items-center justify-center gap-4">
       <form onSubmit={onSubmit} className="w-full flex flex-col gap-2 border-[1px] border-solid border-slate-400 p-2">
-        <h3 className="font-medium">위클리 검색</h3>
+        <h3 className="font-medium text-lg">위클리 검색</h3>
         <div className="w-full flex gap-4 flex-wrap">
           <label className="flex gap-2">
-            <p className="text-sm">수업일자</p>
+            <p>수업일자</p>
             <input
-              className="bg-transparent text-sm border-b-[1px] border-solid border-b-slate-400"
+              className="bg-transparent border-b-[1px] border-solid border-b-slate-400"
               placeholder="YYYYMMDD"
               name="date"
-              type="text"
+              type="date"
               required
             />
           </label>
           <label className="flex gap-2">
-            <p className="text-sm">스터디 그룹명</p>
+            <p>스터디 그룹명</p>
             <input
-              className="bg-transparent text-sm border-b-[1px] border-solid border-b-slate-400"
+              className="bg-transparent border-b-[1px] border-solid border-b-slate-400"
               name="groupName"
               type="text"
               required
@@ -72,7 +74,7 @@ export default function Topics() {
         </div>
         <div className="flex items-center justify-end">
           <button type="submit" className="hover:[&_p]:font-medium">
-            <p className="text-sm">검색</p>
+            <p>검색</p>
           </button>
         </div>
       </form>
@@ -83,15 +85,15 @@ export default function Topics() {
               {idx + 1}. <span>{category}</span>
             </p>
             <div className="w-full flex gap-x-2 gap-y-1 flex-wrap">
-              <div className="w-fit text-sm px-1 rounded-sm whitespace-nowrap overflow-ellipsis bg-indigo-100 dark:bg-indigo-500 dark:bg-opacity-40">
+              <div className="w-fit px-1 rounded-sm whitespace-nowrap overflow-ellipsis bg-indigo-100 dark:bg-indigo-500 dark:bg-opacity-40">
                 <p>{type}</p>
               </div>
-              <div className="w-fit text-sm px-1 rounded-sm whitespace-nowrap overflow-ellipsis bg-violet-100 dark:bg-violet-500 dark:bg-opacity-40">
+              <div className="w-fit px-1 rounded-sm whitespace-nowrap overflow-ellipsis bg-violet-100 dark:bg-violet-500 dark:bg-opacity-40">
                 <p>{subtype}</p>
               </div>
             </div>
             <div className="w-full my-2 h-px bg-slate-400" />
-            <p className="text-sm">{content}</p>
+            <p>{content}</p>
           </div>
         ))}
       </div>
