03f0a4cc321dbcdbd5027cc243daededbfc30f2f
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-01-11 03:38:05 +0900

    css: change bg-color

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function BasicLayout() {
   return (
-    <div className="w-[100vw] h-[100vh] overflow-hidden flex flex-col relative bg-slate-50 dark:bg-slate-800">
+    <div className="w-[100vw] h-[100vh] overflow-hidden flex flex-col relative bg-gradient-to-tr from-indigo-50 to-blue-50 dark:from-slate-900 dark:to-slate-800">
       <Navbar />
       <div className="w-full h-[calc(100vh-56px)] flex flex-col items-center">
         <Outlet />
